7923299127fc3e53a30d8167d2180fe7730cd7cc
DanielRobles1 <162934388+DanielRobles1@users.noreply.github.com>
2024-10-29 23:39:47 -0600

    app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { faSomeIcon } from '@fortawesome/free-brands-svg-icons';
 
